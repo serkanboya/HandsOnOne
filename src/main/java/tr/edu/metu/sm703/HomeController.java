1926dc21ce6390f8c84a7f2a89a7ac4501e35a01
serkanboya <101734770+serkanboya@users.noreply.github.com>
2023-05-24 21:02:12 +0300

    Update HomeController.java

--- src/main/java/tr/edu/metu/sm703/HomeController.java
+++ src/main/java/tr/edu/metu/sm703/HomeController.java
@@ -9,6 +9,6 @@ public class HomeController {
 
     @Get
     public Map<String, Object> index() {
-        return Collections.singletonMap("message", "Hello World");
+        return Collections.singletonMap("message", "Hello World develop");
     }
 }
